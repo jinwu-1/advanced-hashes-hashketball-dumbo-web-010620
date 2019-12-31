20a097d4d1eee8b598aef02db424971e95fccdca
Jin Wu <jinn.wuu@gmail.com>
2019-12-31 18:11:10 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -192,7 +192,8 @@ def player_stats(players_name)
       if attributes == :players
         data.each do |player|
           if player[:player_name] == players_name
-            new_hash = player
+            new_hash = player.delete_if do |k, v|
+              k == :player_name
           end
         end
       end
