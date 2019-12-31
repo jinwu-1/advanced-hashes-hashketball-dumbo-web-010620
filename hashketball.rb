a777cc8c13488f895d4133d357627e1e74c1c160
Jin Wu <jinn.wuu@gmail.com>
2019-12-31 17:44:56 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -140,3 +140,17 @@ def num_points_scored(players_name)
     end
   end
 end
+
+def shoe_size(players_name)
+  game_hash.each do |place, team|
+    team.each do |attribute, data|
+      if attribute == :players
+        data.each do |player|
+          if player[:player_name] == players_name
+            return player[:shoe]
+          end
+        end
+      end
+    end
+  end
+end
